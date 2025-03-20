edc1692ef1d0775b4486a3cf173033a62d2f1f62
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-03-20 08:51:56 -0400

    printing func decl

--- dbl-0.2/src/middleend/ir/ir_print.c
+++ dbl-0.2/src/middleend/ir/ir_print.c
@@ -4,9 +4,30 @@
 
 #include <std/assert/assert.h>
 
+void ir_print_printfuncparameter(ir_translationunit_t* ir, ir_declaration_variable_t* decl)
+{
+    assert(ir);
+    assert(decl);
+
+    printf("%s %s", decl->type.name, decl->name);
+}
+
 void ir_print_printfuncdeclaration(ir_translationunit_t* ir, ir_declaration_function_t* decl)
 {
-    printf("function %s \n", decl->name);
+    int i;
+
+    assert(ir);
+    assert(decl);
+
+    printf("function %s %s ", decl->type.name, decl->name);
+
+    for(i=0; i<decl->parameters.size; i++)
+    {
+        ir_print_printfuncparameter(ir, &decl->parameters.data[i]);
+        printf(" ");
+    }
+
+    printf("\n");
 }
 
 void ir_print_printdeclaration(ir_translationunit_t* ir, ir_declaration_t* decl)
